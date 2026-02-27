115a99d7a34c493ad2e90092ca3e2e4dad8b4fc7
DeepakLabade <deepaklabade001@gmail.com>
2026-02-27 11:23:48 +0530

    added stage 3,4 and 5

--- airtel-router-app-main/backend/src/routes/admin.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { storeMasterForm } from "../controllers/admin/store-master-form.js";
-
-const adminRouter = Router()
-
-adminRouter.post("/master-form", storeMasterForm)
-
-export default adminRouter
